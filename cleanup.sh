971d1c1054ef3d61f79b64c4c22d2ca6f689ff62
David Salgado <david.salgado@digital.justice.gov.uk>
2016-09-12 13:32:28 +0100

    refactor cleanup script
    Get the access key from the stored credentials
    file

--- cleanup.sh
+++ cleanup.sh
@@ -2,15 +2,50 @@
 
 source ./shared.sh
 
-ACCESS_KEY_ID=AKIAJKWDW3X6IMA3B3TA
-
 main() {
-  aws s3 rb s3://${BUCKET} --region ${REGION}
+  local readonly access_key=$(cat ${UPLOAD_CREDS_FILE} | jq -r '.AccessKey.AccessKeyId')
+
+  delete_bucket ${BUCKET} ${REGION}
   # You must delete the user's credentials before you can delete the user
-  aws iam delete-access-key --user-name ${IAM_USER} --access-key-id ${ACCESS_KEY_ID} --region ${REGION}
-  aws iam delete-user --user-name ${IAM_USER} --region ${REGION}
-  rm credentials.json
+  delete_user_credentials ${IAM_USER} ${access_key} ${REGION}
+  delete_user ${IAM_USER} ${REGION}
+  remove_credentials_file ${UPLOAD_CREDS_FILE}
   echo "Finished"
+}
+
+delete_bucket() {
+  local readonly s3_bucket=$1
+  local readonly aws_region=$2
+
+  echo "Delete bucket ${s3_bucket}............"
+
+  aws s3 rb s3://${s3_bucket} --region ${aws_region}
+}
+
+delete_user_credentials() {
+  local readonly user=$1
+  local readonly access_key=$2
+  local readonly aws_region=$3
+
+  echo "Delete user credentials ${access_key}............."
+
+  aws iam delete-access-key --user-name ${user} --access-key-id ${access_key} --region ${aws_region}
+}
+
+delete_user() {
+  local readonly user=$1
+  local readonly aws_region=$2
+
+  echo "Delete user ${user}............."
+
+  aws iam delete-user --user-name ${user} --region ${aws_region}
+}
+
+remove_credentials_file() {
+  local readonly creds_file=$1
+
+  echo "Remove credentials file ${creds_file}........"
+  rm ${creds_file}
 }
 
 main
