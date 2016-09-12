74666b978948fae8c21d2d8e8dcda18abb436697
David Salgado <david.salgado@digital.justice.gov.uk>
2016-09-12 13:42:25 +0100

    move setup functions to shared.sh

--- shared.sh
+++ shared.sh
@@ -59,3 +59,62 @@ sleep_for() {
   printf "\n"
 }
 
+make_bucket() {
+  local readonly bucket=$1
+  local readonly aws_region=$2
+
+  echo "Creating bucket: ${bucket}"
+  aws s3 mb s3://${bucket} --region ${aws_region}
+}
+
+set_bucket_policy() {
+  local readonly bucket=$1
+  local readonly user=$2
+  local readonly policy_template=$3
+  local readonly aws_region=$4
+
+  create_user ${user} ${aws_region}
+  sleep_for 15 # without this, we get "An error occurred (MalformedPolicy) when calling the PutBucketPolicy operation: Invalid principal in policy"
+  # TODO: replace this sleep with a loop that waits until the user information can be retrieved
+  add_bucket_policy ${bucket} ${user} ${policy_template} ${aws_region}
+}
+
+output_credentials() {
+  local readonly user=$1
+  local readonly aws_region=$2
+  local readonly creds_file=$3
+
+  echo "AWS USER CREDENTIALS....................."
+  aws iam create-access-key --user-name ${user} --region ${aws_region} | tee ${creds_file}
+  echo "THESE ARE NOW STORED IN THE FILE: ${creds_file}"
+  echo "PLEASE TAKE APPROPRIATE PRECAUTIONS TO SECURE THESE."
+}
+
+create_user() {
+  local readonly user=$1
+  local readonly aws_region=$2
+
+  echo "Creating IAM user: ${user}"
+  aws iam create-user --user-name ${user} --region ${aws_region}
+}
+
+add_bucket_policy() {
+  export bucket=$1
+  local readonly user=$2
+  local readonly policy_template=$3
+  local readonly aws_region=$4
+
+  export user_arn=$(get_user_arn ${user} ${aws_region})
+  echo "Applying bucket policy"
+  cat ${policy_template} | envsubst > /tmp/policy.json
+  aws s3api put-bucket-policy --bucket ${bucket} --region ${aws_region} --policy file:///tmp/policy.json
+}
+
+get_user_arn() {
+  local readonly user=$1
+  local readonly aws_region=$2
+
+  # Note, this echoes '"value"' not 'value' - i.e. including the double-quotes
+  aws iam get-user --user-name ${user} --region ${aws_region} | jq '.User.Arn'
+}
+
