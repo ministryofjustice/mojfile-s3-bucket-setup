6ea9cbc1738b2e3010c2e5b3f83a1c3cdb2b1a4b
David Salgado <david.salgado@digital.justice.gov.uk>
2016-09-12 13:36:06 +0100

    move cleanup functions to shared.sh

--- shared.sh
+++ shared.sh
@@ -8,3 +8,43 @@ UPLOAD_POLICY_TEMPLATE=${UPLOAD_POLICY_TEMPLATE:-upload-policy.json.template}
 
 UPLOAD_CREDS_FILE=upload-credentials.json
 
+get_access_key() {
+  local readonly creds_file=$1
+  echo $(cat ${creds_file} | jq -r '.AccessKey.AccessKeyId')
+}
+
+delete_bucket() {
+  local readonly s3_bucket=$1
+  local readonly aws_region=$2
+
+  echo "Delete bucket ${s3_bucket}............"
+
+  aws s3 rb s3://${s3_bucket} --region ${aws_region}
+}
+
+delete_user_credentials() {
+  local readonly user=$1
+  local readonly access_key=$2
+  local readonly aws_region=$3
+
+  echo "Delete user credentials ${access_key}............."
+
+  aws iam delete-access-key --user-name ${user} --access-key-id ${access_key} --region ${aws_region}
+}
+
+delete_user() {
+  local readonly user=$1
+  local readonly aws_region=$2
+
+  echo "Delete user ${user}............."
+
+  aws iam delete-user --user-name ${user} --region ${aws_region}
+}
+
+remove_credentials_file() {
+  local readonly creds_file=$1
+
+  echo "Remove credentials file ${creds_file}........"
+  rm ${creds_file}
+}
+
